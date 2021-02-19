144be8f44d6bb5215ad7c689ab221c7b973a3815
jacobgad <jacob.gad@hotmail.com>
2021-02-19 12:14:18 +1100

    Expand the Action notes textbox

---
+++ action-helper.js
@@ -0,0 +1,7 @@
+//description.fieldControl-text-box-text
+console.log('in action helper script');
+setTimeout(() => {
+	let actionNotes = $('textarea[data-id="description.fieldControl-text-box-text"]');
+	console.log('-******************************', actionNotes.val());
+	actionNotes.height('55vh');
+}, 500);
