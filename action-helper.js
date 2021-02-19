a07624baf8bf8ed757f21adcbb3085281c0ac5cb
jacobgad <jacob.gad@hotmail.com>
2021-02-19 16:53:29 +1100

    add document ready function to action helper

--- action-helper.js
+++ action-helper.js
@@ -1,6 +1,8 @@
 console.log('in action helper script');
-setTimeout(() => {
-	let actionNotes = $('textarea[data-id="description.fieldControl-text-box-text"]');
-	console.log('-******************************', actionNotes.val());
-	actionNotes.height('55vh');
-}, 500);
+$(document).ready(function () {
+	setTimeout(() => {
+		let actionNotes = $('textarea[data-id="description.fieldControl-text-box-text"]');
+		console.log('-******************************', actionNotes.val());
+		actionNotes.height('55vh');
+	}, 500);
+});
